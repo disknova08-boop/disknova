07d61aeaac22eafb840d02ebad732cd9f00b655f
DiskNova <disknova08@gmail.com>
2025-11-14 23:53:52 +0530

    fix: added correct Supabase final4

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "451284055"
+    serviceWorkerVersion: "2104719823"
   }
 });
